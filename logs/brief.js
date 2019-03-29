34a067ed8ebe6178063d867451755c69564125fb
unknown <giang@nguyen.D-LINK.local>
2019-03-29 15:18:32 +0900

    03292019

--- logs/brief.js
+++ logs/brief.js
@@ -3,3 +3,4 @@ var brief = new Object()
 brief["20190325"] = "create log page";
 brief["20190326"] = "html<br>3D ML Resource<br>ZenkeiAI 03/2019";
 brief["20190328"] = "ZenkeiAI 03/2019 (continue)";
+brief["20190329"] = "recommender systems";
