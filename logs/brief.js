8ea8a558c813f86bb100d6a0bc54784a782ae712
unknown <giang@nguyen.D-LINK.local>
2019-04-11 11:32:29 +0900

    04102019

--- logs/brief.js
+++ logs/brief.js
@@ -11,3 +11,4 @@ brief["20190404"] = "Project01-(adding Gaussian noise)";
 brief["20190405"] = "Project01-(some FC layer for angle)";
 brief["20190408"] = "Project01-(continue)";
 brief["20190409"] = "Project01-(check the original data)";
+brief["20190410"] = "Project01-(angle 0 - regression)";
